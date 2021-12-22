9183f502cfd8c713ddac49aa310582c411bd20a3
ts4blader <ts4blader@gmail.com>
2021-12-23 00:18:39 +0700

    add link to logo and style

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Image from "./Image";
 import NavMobile from "./NavMobile";
+import { useHistory } from "react-router-dom";
 
 export default function Nav() {
+  const history = useHistory();
+
   return (
     <nav className="nav container">
-      <div className="nav__right">
+      <div className="nav__right" onClick={() => history.push("/")}>
         <Image src="logo-light.png" alt="Logo" />
       </div>
       <div className="nav__left">
