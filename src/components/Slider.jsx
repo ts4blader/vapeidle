c18184b608766f6129d451d0f7094bc3a0e969f7
ts4blader <ts4blader@gmail.com>
2021-12-24 20:00:34 +0700

    add link

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import Glide from "@glidejs/glide";
+import { Link } from "react-router-dom";
 
 export default function Slider({ options, data = [], id }) {
   useEffect(() => {
@@ -13,7 +14,7 @@ export default function Slider({ options, data = [], id }) {
         <div className="glide__bullets" data-glide-el="controls[nav]">
           {data.map((item, index) => (
             <button
-              key={`${id}-pagination-${index}`}
+              key={`${item.id}-pagination`}
               className="glide__bullet"
               data-glide-dir={`=${index}`}
             ></button>
@@ -23,14 +24,16 @@ export default function Slider({ options, data = [], id }) {
         <div className="glide__track" data-glide-el="track">
           <ul className="glide__slides">
             {data.map((item, index) => (
-              <li className="glide__slide" key={item.id}>
-                <img src={item.img} alt="placeholder" />
-                <div className="basic-info">
-                  <div className="no">{`0${index + 1}`}</div>
-                  <div className="bar"></div>
-                  <div className="categories">{item.categories}</div>
-                </div>
-                <div className="name">{item.name}</div>
+              <li className="glide__slide" key={item.id + "slider"}>
+                <Link to={`/products/${item.slug}`}>
+                  <img src={item.img} alt="placeholder" />
+                  <div className="basic-info">
+                    <div className="no">{`0${index + 1}`}</div>
+                    <div className="bar"></div>
+                    <div className="categories">{item.categories}</div>
+                  </div>
+                  <div className="name">{item.name}</div>
+                </Link>
               </li>
             ))}
           </ul>
