376966b71ed1b77e911e8dbd95d2ee6e8315d652
ts4blader <ts4blader@gmail.com>
2021-12-26 14:53:48 +0700

    add link to products

--- src/components/ProductsShow.jsx
+++ src/components/ProductsShow.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function ProductsShow({ data }) {
   return (
@@ -6,9 +7,11 @@ export default function ProductsShow({ data }) {
       <ul className="products__list">
         {data?.map((item) => (
           <li key={`products-show-${item.id}`}>
-            <img src={item.img} className="thumbnail" alt={item.name} />
-            <h3 className="name">{item.name}</h3>
-            <div className="price">{item.price + "$"}</div>
+            <Link to={`/products/${item.slug}`}>
+              <img src={item.img} className="thumbnail" alt={item.name} />
+              <h3 className="name">{item.name}</h3>
+              <div className="price">{item.price + "$"}</div>
+            </Link>
           </li>
         ))}
       </ul>
